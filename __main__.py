07a3f86a5bcfe6d6990f8a5536033a44e7003109
Raphaël Landuré <raph.landure@gmail.com>
2020-04-18 21:02:11 +0200

    Blinker and Toad oscillators

--- __main__.py
+++ __main__.py
@@ -1,12 +1,12 @@
 import click
 
-from cells import living_cells
-from grid import grid_maker
+from .cells import living_cells
+from .grid import grid_maker
 
 import matplotlib.pyplot as plt
 
 
-def show_grid(universe):
+def show_grid(universe: grid_maker):
     plt.imshow(universe, cmap='binary')
     plt.show()
 
@@ -15,14 +15,25 @@ def show_grid(universe):
 @click.option("--grid_size", "-gs", "grid_size", type=int, default=100)
 @click.option("--random_init", "-ri", "random_init", type=bool, default=False)
 @click.option("--random_init_length", "-ril", "random_init_length", type=int, default=10)
-def main(grid_size: int, random_init: bool, random_init_length: int):
+@click.option("--beacon", "-beacon", "beacon", type=bool, default=False)
+@click.option("--blinker", "-blinker", "blinker", type=bool, default=False)
+@click.option("--toad", "-toad", "toad", type=bool, default=False)
+def main(grid_size: int, random_init: bool, random_init_length: int, beacon: bool, blinker: bool, toad: bool) -> None:
     universe = grid_maker.Grid(grid_size).build_grid()
 
     if random_init:
-        random_initialisation = living_cells.LivingCells(universe=universe).random_init(random_init_length=random_init_length)
-        show_grid(random_initialisation)
+        random_initialization = living_cells.LivingCells(universe=universe).random_init(random_init_length=random_init_length)
+        show_grid(random_initialization)
     else:
-        pass
+        if beacon:
+            beacon_initialization = living_cells.LivingCells(universe=universe).beacon()
+            show_grid(beacon_initialization)
+        if blinker:
+            blinker_initialization = living_cells.LivingCells(universe=universe).blinker()
+            show_grid(blinker_initialization)
+        if toad:
+            toad_initialization = living_cells.LivingCells(universe=universe).toad()
+            show_grid(toad_initialization)
 
 
 if __name__ == '__main__':
--- cells/living_cells.py
+++ cells/living_cells.py
@@ -10,9 +10,9 @@ class LivingCells:
         """
         self.universe = universe
 
-    def random_init(self, random_init_length: int):
+    def random_init(self, random_init_length: int) -> np.ndarray:
         """
-        Functions that implements a random initialization for the Conway's
+        Method that implements a random initialization for the Conway's
         Game-of-Life.
         :param random_init_length: length of the random initialization
         :return: grid with a random centered initialization
@@ -31,15 +31,51 @@ class LivingCells:
                 len(self.universe) - int((len(self.universe) - random_init_length) / 2))] = init
         return grid
 
-    def beacon(self):
+    def beacon(self) -> np.ndarray:
+        """
+        Method that implements a Beacon oscillator.
+        :return: universe with a beacon at its center
+        """
         grid = self.universe
         beacon = [[1, 1, 0, 0],
-                [1, 1, 0, 0],
-                [0, 0, 1, 1],
-                [0, 0, 1, 1]]
-        grid[int((len(self.universe) - len(beacon)) / 2):(
-                len(self.universe) - int((len(self.universe) - len(beacon)) / 2)),
-        int((len(self.universe) - len(beacon)) / 2):(
-                len(self.universe) - int((len(self.universe) - len(beacon)) / 2))] = beacon
+                  [1, 1, 0, 0],
+                  [0, 0, 1, 1],
+                  [0, 0, 1, 1]]
+        if len(grid) % 2 == 0:
+            grid[int((len(grid) - len(beacon)) / 2):(
+                    len(grid) - int((len(grid) - len(beacon)) / 2)),
+            int((len(grid) - len(beacon)) / 2):(
+                    len(grid) - int((len(grid) - len(beacon)) / 2))] = beacon
+        else:
+            grid[int((len(grid) - len(beacon)) / 2):(
+                    len(grid) - int((len(grid) - len(beacon)) / 2 + 1)),
+            int((len(grid) - len(beacon)) / 2):(
+                    len(grid) - int((len(grid) - len(beacon)) / 2 + 1))] = beacon
         return grid
 
+    def blinker(self) -> np.ndarray:
+        """
+        Method that implements a Blinker oscillator.
+        :return: universe with a blinker at its center
+        """
+        grid = self.universe
+        blinker = [1, 1, 1]
+        if len(grid) % 2 == 0:
+            grid[int(len(grid) / 2) - 1, int(len(grid) / 2) - 1:int(len(grid) / 2) + 2] = blinker
+        else:
+            grid[int(len(grid) / 2), int(len(grid) / 2) - 1:int(len(grid) / 2) + 2] = blinker
+        return grid
+
+    def toad(self) -> np.ndarray:
+        """
+        Method that implements a Toad oscillator.
+        :return: universe with a toad at its center
+        """
+        grid = self.universe
+        toad = [[0, 1, 1, 1],
+                [1, 1, 1, 0]]
+        if len(grid) % 2 == 0:
+            grid[int(len(grid) / 2):(int(len(grid) / 2) + 1), (int((len(grid) - len(toad[0])) / 2)):(int((len(grid) - len(toad[0])) / 2) + 3)] = toad
+        else:
+            pass
+        return grid
